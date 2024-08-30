5b6536ab9b08517e74cbbd53df125392d11d67ca
Sonali Srivastava <srivastava.sonali1@gamil.com>
2024-08-30 16:40:55 +0530

    added test and manifest for deployment

---
+++ test/cypress/executor-tests/cypress-13/cypress/e2e/cypress-test.cjs
@@ -0,0 +1,9 @@
+describe('HelloKube App', () => {
+  it('should display the header and main content', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('header h1').should('contain.text', 'HelloKube App');
+    cy.get('section p').should('contain.text', 'Welcome to the HelloKube App.');
+  });
+});og(`Server running on http://localhost:${PORT}/`);
+});
