1c1c98ae7307532ea51b08910cb239dc7fe6d3f9
Sonali Srivastava <srivastava.sonali1@gamil.com>
2024-08-30 16:53:52 +0530

    fixed URL

--- test/cypress/executor-tests/cypress-13/cypress/e2e/cypress-test.cy.js
+++ test/cypress/executor-tests/cypress-13/cypress/e2e/cypress-test.cy.js
@@ -1,6 +1,6 @@
 describe('HelloKube App', () => {
   it('should display the header and main content', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://192.168.103.2:30865');
 
     cy.get('header h1').should('contain.text', 'HelloKube App');
     cy.get('section p').should('contain.text', 'Welcome to the HelloKube App.');
