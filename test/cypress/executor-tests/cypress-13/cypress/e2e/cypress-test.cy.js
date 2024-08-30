53645da8b267dc1a9ef327d2b03a88ef655887c4
Sonali Srivastava <srivastava.sonali1@gamil.com>
2024-08-30 17:47:02 +0530

    updated the test

--- test/cypress/executor-tests/cypress-13/cypress/e2e/cypress-test.cy.js
+++ test/cypress/executor-tests/cypress-13/cypress/e2e/cypress-test.cy.js
@@ -1,6 +1,6 @@
 describe('HelloKube App', () => {
   it('should display "Hello, Kubernetes!" for any other route', () => {
-    cy.request('http://192.168.103.2:30865/');
+    cy.request('http://localhost:3000/');
 
     cy.contains('Hello, Kubernetes!').should('be.visible');
   });
