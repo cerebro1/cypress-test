e74d77708eaa56d2387e494a46e2f13367e5fe82
Sonali Srivastava <srivastava.sonali1@gamil.com>
2024-08-30 17:17:20 +0530

    updated the test

--- test/cypress/executor-tests/cypress-13/cypress/e2e/cypress-test.cy.js
+++ test/cypress/executor-tests/cypress-13/cypress/e2e/cypress-test.cy.js
@@ -1,8 +1,6 @@
 describe('HelloKube App', () => {
   it('should display the header and main content', () => {
     cy.request('http://192.168.103.2:30865');
-
-    cy.get('header h1').should('contain.text', 'HelloKube App');
-    cy.get('section p').should('contain.text', 'Welcome to the HelloKube App.');
+    cy.contains('Hello, Kubernetes!').should('be.visible');
   });
 });
